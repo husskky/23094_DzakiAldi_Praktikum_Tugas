ec70ee3ac84fa8fc392006e2c173479341eb7a5e
husskky <89946765+husskky@users.noreply.github.com>
2024-09-17 20:57:34 +0700

    Add files via upload

---
+++ 23094_DzakiAldi_Praktikum_Tugas2/TugasP3/SoalNo1/MatematikaInheritance.java
@@ -0,0 +1,12 @@
+package TugasP3.SoalNo1;
+
+public class MatematikaInheritance {
+    public static void main(String[] args) {
+        Matematika2 mtk = new Matematika2();
+        mtk.pertambahan(1, 62);
+        mtk.pengurangan(20, 12);
+        mtk.perkalian(20, 4);
+        mtk.pembagian(50, 10);
+        mtk.modulus(20, 2);
+    }
+}
---
+++ 23094_DzakiAldi_Praktikum_Tugas2/TugasP3/SoalNo2/DemoKonversiSuhu.java
@@ -0,0 +1,10 @@
+package TugasP3.SoalNo2;
+
+public class DemoKonversiSuhu {
+    public static void main(String[] args) {
+        KonversiSuhu2 ks = new KonversiSuhu2();
+        ks.celciusToFahrenheit(41);
+        ks.celciusToReamur(55);
+        ks.farhenheitToReamur(77);
+    }
+}
