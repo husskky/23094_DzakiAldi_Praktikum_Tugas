dacbe239e3ceff90e5d1c3ab057a17f3243fe1a8
husskky <89946765+husskky@users.noreply.github.com>
2024-11-06 01:40:03 +0700

    Add files via upload

---
+++ 23094_DzakiAldi_Praktikum_Tugas2/src/TugasP10/InputDataMahasiswa.java
@@ -0,0 +1,24 @@
+package TugasP10;
+//import com.sun.jdi.connect.spi.TransportService;
+import java.util.ArrayList;
+
+public class InputDataMahasiswa {    
+    ArrayList<Mahasiswa>listmahasiswa;
+   
+    public InputDataMahasiswa(){
+        listmahasiswa = new ArrayList();
+    }
+    
+    public void insertData(String NIM, String Nama, String Alamat, String MKul, String nilaiAkhir){
+        Mahasiswa mhs = new Mahasiswa(NIM, Nama, Alamat, MKul, nilaiAkhir);
+        listmahasiswa.add(mhs);
+    }
+  
+    public ArrayList<Mahasiswa>getALL(){
+        return listmahasiswa;
+    }
+    public void deleteData(int index){
+        listmahasiswa.remove(index);
+    }
+
+}
